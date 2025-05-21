3be7664b9cae7d09b483d88aff5541a83ec4101b
sagar-bankar <sagar.bankar590@gmail.com>
2025-05-21 08:39:16 +0530

    commit------>21 May 2025

--- src/test/java/com/parabank/utils/ExtentReportManager.java
+++ src/test/java/com/parabank/utils/ExtentReportManager.java
@@ -1,5 +1,6 @@
 package com.parabank.utils;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,75 +14,82 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.parabank.tests.BaseClass;
 
-
-
 public class ExtentReportManager implements ITestListener {
 
-    // Declare ExtentReports and ExtentTest objects
-    
+	// Declare ExtentReports and ExtentTest objects
+
 	private ExtentReports extent;
-    private ExtentTest test;
+	private ExtentTest test;
 
-    public void onStart(ITestContext context) {
-        // This method is called before any test method is run
-        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()); // Time stamp
-       String reportFileName = "Test-report-" + timeStamp + ".html"; // Report file name
+	public void onStart(ITestContext context) {
+		// This method is called before any test method is run
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()); // Time stamp
+		String reportFileName = "Test-report-" + timeStamp + ".html"; // Report file name
 
-        // Initialize ExtentSparkReporter with the path where the report will be saved
-        ExtentSparkReporter htmlReporter = new ExtentSparkReporter("C:\\Workspaces\\30-10-2024 On words\\Parabank_V1.2\\reports\\"+reportFileName);
-        htmlReporter.config().setDocumentTitle("Automation Test Report"); // Title of the report
-        htmlReporter.config().setReportName("Functional Test Report"); // Name of the report
-       // htmlReporter.config().setTheme(Theme.STANDARD); // Theme of the report
-        htmlReporter.config().setTheme(Theme.DARK); // Theme of the report
+		// Initialize ExtentSparkReporter with the path where the report will be saved
+		ExtentSparkReporter htmlReporter = new ExtentSparkReporter(
+				"C:\\Workspaces\\30-10-2024 On words\\Parabank_V1.2\\reports\\" + reportFileName);
+		htmlReporter.config().setDocumentTitle("ParaBank V1.2 Test Report"); // Title of the report
+		htmlReporter.config().setReportName("Functional Test Report"); // Name of the report
+		
+		
+		// htmlReporter.config().setTheme(Theme.STANDARD); // Theme of the report
+		htmlReporter.config().setTheme(Theme.DARK); // Theme of the report
 
-        // Initialize ExtentReports and attach the reporter
-        extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
+		// Initialize ExtentReports and attach the reporter
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
 
-        // Set additional information about the environment
-        extent.setSystemInfo("Host Name", "Your Host Name");
-        extent.setSystemInfo("Environment", "QA");
-        extent.setSystemInfo("User Name", "SAGAR BANKAR");
-    }
+		// Set additional information about the environment
+		// extent.setSystemInfo("Host Name", "Your Host Name");
+		// extent.setSystemInfo("Environment", "QA");
+		// extent.setSystemInfo("User Name", "SAGAR BANKAR");
 
-    public void onTestStart(ITestResult result) {
-        // This method is called when a test starts
-        test = extent.createTest(result.getTestClass().getName()); // Create a new test in the report
-    }
+		// Infosys level doc
+		extent.setSystemInfo("OS", "Windows");
+		extent.setSystemInfo("Browser", "Chrome");
+		extent.setSystemInfo("Java Version", "1.8/11");
+		extent.setSystemInfo("Build URL", "http://localhost:8080/job/ParaBank_Automation/23/");
+		extent.setSystemInfo("Host Name", "Jenkins_Agent_01");
+		extent.setSystemInfo("Environment", "QA");
+		extent.setSystemInfo("Executed By", "SAGAR BANKAR");
 
-    public void onTestSuccess(ITestResult result) {
-        // This method is called when a test is successful
-        test.pass("Test Passed"); // Log a pass message in the report
-    }
+	}
 
-    public void onTestFailure(ITestResult result) {
-        // This method is called when a test fails
-        test.fail(result.getThrowable()); // Log the failure exception in the report
-    
-    
-        // BaseClass baseclass=new BaseClass();
-        
-      	  String imgPath=new BaseClass().captureScreen(result.getName());
-            test.addScreenCaptureFromPath(imgPath);
-  	    
-  	    
-        
-        
-        
-    }
+	public void onTestStart(ITestResult result) {
+		// This method is called when a test starts
+		test = extent.createTest(result.getTestClass().getName()); // Create a new test in the report
+	}
 
-    public void onTestSkipped(ITestResult result) {
-        // This method is called when a test is skipped
-        test.skip(result.getThrowable()); // Log the skip exception in the report
-    }
+	public void onTestSuccess(ITestResult result) {
+		// This method is called when a test is successful
+		test.pass("Test Passed"); // Log a pass message in the report
+	}
 
-    public void onFinish(ITestContext context) {
-        // This method is called after all test methods have been run
-        extent.flush(); // Write the test results to the report
-    }
+	public void onTestFailure(ITestResult result) {
+		// This method is called when a test fails
+		test.fail(result.getThrowable()); // Log the failure exception in the report
 
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        // This method is called for tests that fail but are within the success percentage defined in TestNG
-        // Not commonly used, so implementation can be skipped or customized as needed
-    }
+		// BaseClass baseclass=new BaseClass();
+
+		String imgPath = new BaseClass().captureScreen(result.getName());
+		test.addScreenCaptureFromPath(imgPath);
+
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// This method is called when a test is skipped
+		test.skip(result.getThrowable()); // Log the skip exception in the report
+	}
+
+	public void onFinish(ITestContext context) {
+		// This method is called after all test methods have been run
+		extent.flush(); // Write the test results to the report
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// This method is called for tests that fail but are within the success
+		// percentage defined in TestNG
+		// Not commonly used, so implementation can be skipped or customized as needed
+	}
 }
